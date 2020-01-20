42fa7a2c8ac36954a02fe8b8bd0f2e591ef29370
Weiran Fu <wfu4@engr-ras-102.eos.ncsu.edu>
2020-01-19 23:30:38 -0500

    main() done

--- In-Memory vs. Disk-based Searching/src/main.c
+++ In-Memory vs. Disk-based Searching/src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include "mem_lin.h"
 #include "mem_bin.h"
 #include "disk_lin.h"
 #include "disk_bin.h"
 
 int main(int argc, const char * argv[]) {
-    const char* key = argv[1];
-    const char* seek = argv[2];
-    mem_lin_search(key, seek);    
+    if (argc != 4) {
+        printf("Argument number error\n");
+        return 0;
+    }
+    const char* mode = argv[1];
+    const char* key = argv[2];
+    const char* seek = argv[3];
+    if (strcmp(mode, "--mem-lin") == 0) {
+        mem_lin_search(key, seek);
+    } else if (strcmp(mode, "--mem-bin") == 0) {
+        mem_bin_search(key, seek);
+    } else if (strcmp(mode, "--disk-lin") == 0) {
+        disk_lin_search(key, seek);
+    } else if (strcmp(mode, "--disk-bin") == 0) {
+        disk_bin_search(key, seek);
+    } else {
+        printf("Argument format error\n");
+    }
     return 0;
 }
