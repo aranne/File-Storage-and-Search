b6ba3fad574d7c89faa9714ab1e8247129cd37fb
Weiran Fu <wfu4@engr-ras-200.eos.ncsu.edu>
2020-01-19 19:39:49 -0500

    memory lenear done

--- In-Memory vs. Disk-based Searching/src/mem_lin.c
+++ In-Memory vs. Disk-based Searching/src/mem_lin.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include "mem_lin.h"
 #include "load.h"
 #include "Array.h"
 
 void search(const char* kp, const char* sp) {
+    // record start time
+    struct timeval tm;
+    gettimeofday(&tm, NULL);
+    long int start_sec = tm.tv_sec;
+    long int start_usec = tm.tv_usec;
+
     Array key = load(kp);
     Array seek = load(sp);
     Array hit = array_create(seek.size);
@@ -32,5 +39,11 @@ void search(const char* kp, const char* sp) {
     array_free(&key);
     array_free(&seek);
     array_free(&hit);
+
+    // record end time
+    gettimeofday(&tm, NULL);
+    long int end_sec = tm.tv_sec;
+    long int end_usec = tm.tv_usec;
+    printf("Time: %ld.%06ld\n", end_sec - start_sec, end_usec - start_usec);
 }
 
